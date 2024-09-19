a5fd52843651c0f75b73004a417d7a853665d6e6
Christopher Mann <christopher.mann@dbosoft.eu>
2024-09-19 09:34:00 +0200

    Add confirmation when deleting projects (#82)

--- src/Eryph.ComputeClient.Commands/Projects/RemoveProjectCommand.cs
+++ src/Eryph.ComputeClient.Commands/Projects/RemoveProjectCommand.cs
@@ -1,42 +1,53 @@
 ﻿using System;
 using System.Management.Automation;
-using Eryph.ClientRuntime;
 using Eryph.ComputeClient.Models;
 using JetBrains.Annotations;
 
-namespace Eryph.ComputeClient.Commands.Projects
+namespace Eryph.ComputeClient.Commands.Projects;
+
+[PublicAPI]
+[Cmdlet(VerbsCommon.Remove, "EryphProject")]
+[OutputType(typeof(Operation))]
+public class RemoveProjectCommand : ProjectCmdlet
 {
-    [PublicAPI]
-    [Cmdlet(VerbsCommon.Remove, "EryphProject")]
-    [OutputType(typeof(Operation))]
-    public class RemoveProjectCommand : ProjectCmdlet
+    [Parameter(
+        Position = 0,
+        ValueFromPipeline = true,
+        Mandatory = true,
+        ValueFromPipelineByPropertyName = true)]
+    public string[] Id { get; set; }
+
+    [Parameter]
+    public SwitchParameter Force { get; set; }
+
+    [Parameter]
+    public SwitchParameter NoWait { get; set; }
+
+    private bool _yesToAll;
+    private bool _noToAll;
+
+    protected override void ProcessRecord()
     {
-        [Parameter(
-            Position = 0,
-            ValueFromPipeline = true,
-            Mandatory = true,
-            ValueFromPipelineByPropertyName = true)]
-        public string[] Id { get; set; }
-
-        [Parameter]
-        public SwitchParameter NoWait
+        foreach (var id in Id)
         {
-            get => _nowait;
-            set => _nowait = value;
-        }
-
-        private bool _nowait;
-
-
-        protected override void ProcessRecord()
-        {
-            foreach (var id in Id)
+            Project project;
+            try
             {
-                WaitForProject(Factory.CreateProjectsClient().Delete(id)
-                    , _nowait, false, id);
+                project = Factory.CreateProjectsClient().Get(id);
+            }
+            catch (Exception ex)
+            {
+                WriteError(new ErrorRecord(ex, "ProjectNotFound", ErrorCategory.ObjectNotFound, id));
+                continue;
             }
 
-        }
+            if (!Force && !ShouldContinue($"Project '{project.Name}' (Id:{id}) and all catlets in the project will be deleted!", "Warning!",
+                    ref _yesToAll, ref _noToAll))
+            {
+                continue;
+            }
 
+            WaitForProject(Factory.CreateProjectsClient().Delete(id), NoWait, false, id);
+        }
     }
 }
